ef89ef9b2c0c89e643b815dade1797c0dfe2eadb
arlexz96 <arlex.z96@gmail.com>
2024-08-22 06:36:49 -0500

    Feat: Se Realiza la migración de la base de datos al código

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gestionBiblioteca.Models;
+
+public partial class User
+{
+    public int IdUser { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int? IdRol { get; set; }
+
+    public virtual Role? IdRolNavigation { get; set; }
+}
